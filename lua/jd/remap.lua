9aa53e80ee37f2887c1e122c4cef30b6f2db05a1
Junaid <junaid999999999@gmail.com>
2023-05-29 19:15:38 +0300

    add diagnostic keymaps

--- lua/jd/remap.lua
+++ lua/jd/remap.lua
@@ -1,5 +1,10 @@
 vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
 
+-- system clipboard
 vim.keymap.set("n", "<leader>sy", "\"+y")
 vim.keymap.set("n", "<leader>sp", "\"+p")
 vim.keymap.set("n", "<leader>sY", "\"+Y")
+
+-- diagnostics
+vim.api.nvim_set_keymap('n', '<leader>do', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>dd', '<cmd>Telescope diagnostics<CR>', { noremap = true, silent = true })
