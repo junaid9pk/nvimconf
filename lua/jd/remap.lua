2414967c51faa45bd563a62753f738a4493e9922
Junaid <junaid999999999@gmail.com>
2023-06-04 16:07:13 +0300

    add remaps for window size

--- lua/jd/remap.lua
+++ lua/jd/remap.lua
@@ -5,6 +5,13 @@ vim.keymap.set("n", "<leader>sy", "\"+y")
 vim.keymap.set("n", "<leader>sp", "\"+p")
 vim.keymap.set("n", "<leader>sY", "\"+Y")
 
+-- screen
+vim.keymap.set("n", "<leader>wwi", "<C-w>><CR>")
+vim.keymap.set("n", "<leader>wwd", "<C-w><<CR>")
+vim.keymap.set("n", "<leader>whi", "<C-w>+<CR>")
+vim.keymap.set("n", "<leader>whd", "<C-w>-<CR>")
+
 -- diagnostics
 vim.api.nvim_set_keymap('n', '<leader>do', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>dd', '<cmd>Telescope diagnostics<CR>', { noremap = true, silent = true })
+
