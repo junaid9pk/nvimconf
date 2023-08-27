26c0e464b67338e3fb13d59edb2ce7699b6cbd26
Junaid <junaid96.pk@gmail.com>
2023-08-28 02:31:37 +0300

    telescope find_files if not a git repository

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,25 @@
+local function exists(file)
+   local ok, err, code = os.rename(file, file)
+   if not ok then
+      if code == 13 then
+         -- Permission denied, but it exists
+         return true
+      end
+   end
+   return ok, err
+end
+
+local function isdir(path)
+   -- "/" works on both Unix and Windows
+   return exists(path.."/")
+end
+
 local builtin = require('telescope.builtin')
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+if (isdir(".git")) then
 vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
+else
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+end
 vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
