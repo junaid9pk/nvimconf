53b179e6d2b6ba2e0234f279d5052eb6a2a08f7d
Junaid <junaid96.pk@gmail.com>
2023-12-30 15:49:27 +0300

    add remaps to move text

--- lua/jd/remap.lua
+++ lua/jd/remap.lua
@@ -5,6 +5,14 @@ vim.keymap.set("n", "<leader>sy", "\"+y")
 vim.keymap.set("n", "<leader>sp", "\"+p")
 vim.keymap.set("n", "<leader>sY", "\"+Y")
 
+-- text
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==")
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==")
+vim.keymap.set("v", "<A-k>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<A-j>", ":m '<-2<CR>gv=gv")
+vim.keymap.set("i", "<A-j>", "<Esc>:m .+1<CR>==gi")
+vim.keymap.set("i", "<A-k>", "<Esc>:m .-2<CR>==gi")
+
 -- tabs
 vim.keymap.set("n", "<leader>tn", ":tabnew<CR>")
 vim.keymap.set("n", "<leader>tg", ":tabnext")
