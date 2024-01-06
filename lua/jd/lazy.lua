9c1f7a1accad02f98d32431037893fc9aa27f5ac
Junaid <junaid96.pk@gmail.com>
2024-01-06 07:19:08 +0300

    add lsp

--- lua/jd/lazy.lua
+++ lua/jd/lazy.lua
@@ -11,4 +11,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("jd.plugins")
+require("lazy").setup({ {import = "jd.plugins"}, {import = "jd.plugins.lsp"} }, {
+    checker = {
+        enabled = true,
+        notify = false,
+    },
+    change_detector = {
+        notify = false,
+    }
+})
