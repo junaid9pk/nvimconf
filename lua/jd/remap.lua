d69712b3676ecff401219289d7acf7310ae4a451
Junaid <junaid96.pk@gmail.com>
2024-01-10 18:11:05 +0300

    fix text moving remap

--- lua/jd/remap.lua
+++ lua/jd/remap.lua
@@ -4,10 +4,10 @@ vim.keymap.set("n", "<leader>sp", "\"+p")
 vim.keymap.set("v", "<leader>y", "\"+Y")
 
 -- move text
-vim.keymap.set("n", "<A-k>", ":m .-2<CR>==")
 vim.keymap.set("n", "<A-j>", ":m .+1<CR>==")
-vim.keymap.set("v", "<A-k>", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "<A-j>", ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==")
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv")
 vim.keymap.set("i", "<A-j>", "<Esc>:m .+1<CR>==gi")
 vim.keymap.set("i", "<A-k>", "<Esc>:m .-2<CR>==gi")
 
