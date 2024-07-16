1c44df8e3f7522d456fe5aa425b7fd143d9e872a
Junaid <junaid96.pk@gmail.com>
2024-07-16 03:28:06 +0300

    restored nvim-cmp to an older version and updated settings for windows

--- lua/jd/settings.lua
+++ lua/jd/settings.lua
@@ -9,3 +9,8 @@ if _ENV == 'Ubuntu' then
     SETTINGS['vue_typescript_plugin_path'] = '/home/jd/.nvm/versions/node/v20.13.1/lib/node_modules/@vue/typescript-plugin'
     SETTINGS['typescript_path'] = '/home/jd/.nvm/versions/node/v20.13.1/lib/node_modules/typescript/lib'
 end
+
+if _ENV == 'Windows' then
+    SETTINGS['vue_typescript_plugin_path'] = "C:\\Users\\junai\\AppData\\Roaming\\npm\\node_modules\\@vue\\typescript-plugin"
+    SETTINGS['typescript_path'] = "C:\\Users\\junai\\AppData\\Roaming\\npm\\node_modules\\typescript\\lib"
+end
