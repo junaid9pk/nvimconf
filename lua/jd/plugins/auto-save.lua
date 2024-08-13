1a80233ae0d6beb69c7812305b8b9ff866ec9d9e
Junaid <junaid96.pk@gmail.com>
2024-08-13 03:21:15 +0300

    configured auto-save to exclude harpoon

--- lua/jd/plugins/auto-save.lua
+++ lua/jd/plugins/auto-save.lua
@@ -1,3 +1,21 @@
 return {
     "pocco81/auto-save.nvim",
+    config = function ()
+        require('auto-save').setup(
+            {
+                trigger_events = {"InsertLeave", "TextChanged"},
+                condition = function(buf)
+                    local fn = vim.fn
+                    local utils = require("auto-save.utils.data")
+
+                    if
+                        fn.getbufvar(buf, "&modifiable") == 1 and
+                        utils.not_in(fn.getbufvar(buf, "&filetype"), {"harpoon"}) then
+                        return true
+                    end
+                    return false
+                end,
+            }
+        )
+    end
 }
