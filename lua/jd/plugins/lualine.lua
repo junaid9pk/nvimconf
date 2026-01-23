0836ac1bbce3475cfdd2bb1f04908ac53e8603d6
Junaid <junaid96.pk@gmail.com>
2026-01-24 01:37:53 +0300

    change lualine theme

--- lua/jd/plugins/lualine.lua
+++ lua/jd/plugins/lualine.lua
@@ -13,7 +13,7 @@ return {
         require('lualine').setup {
             options = {
                 icons_enabled = true,
-                theme = 'auto',
+                theme = 'nord',
                 component_separators = { left = '', right = ''},
                 section_separators = { left = '', right = ''},
                 disabled_filetypes = {
